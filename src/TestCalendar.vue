891d413a1a196baa752f3318796d19cbd3369f71
Kingsley <sabistore12@gmail.com>
2019-09-02 18:22:13 +0100

    commit changes made to calendar

--- src/TestCalendar.vue
+++ src/TestCalendar.vue
@@ -1,8 +1,10 @@
 <template>
   <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins"  
   :events="[
+  { title: 'event 4', date: '2019-09-02' },
     { title: 'event 1', date: '2019-09-09' },
-    { title: 'event 2', date: '2019-09-14' }
+    { title: 'event 2', date: '2019-09-10' },
+    { title: 'event 3', date: '2019-09-11' },
   ]"
   />
 </template>
